bbf95b48a5c2f5db26cbe80635f156da74942373
Yura Kovalchuk <deersblabla@gmail.com>
2023-03-26 19:57:26 +0300

    add input feature

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:dart_core_cli/dart_core_cli.dart' as dart_core_cli;
 
 void main(List<String> arguments) {
-  String? number1 = "3";
-  String? number2 = "5";
-  int? result1 = int.tryParse(number1);
-  int? result2 = int.tryParse(number2);
+  String? number1 = stdin.readLineSync();
+  String? number2 = stdin.readLineSync();
+  int? result1 = int.tryParse(number1!);
+  int? result2 = int.tryParse(number2!);
+
   print(result1! + result2!);
 }
