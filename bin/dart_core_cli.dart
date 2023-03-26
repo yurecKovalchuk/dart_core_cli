893208bf21f2b14ad853c1547533d338c07bbbff
Yura Kovalchuk <deersblabla@gmail.com>
2023-03-26 19:27:23 +0300

    finish task with summ two numbers

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -1,5 +1,9 @@
 import 'package:dart_core_cli/dart_core_cli.dart' as dart_core_cli;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_core_cli.calculate()}!');
+  String? number1 = "3";
+  String? number2 = "5";
+  int? result1 = int.tryParse(number1);
+  int? result2 = int.tryParse(number2);
+  print(result1! + result2!);
 }
