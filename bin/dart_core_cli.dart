5c9f44d7f442c7f15aaaae1f77fedc20d532c27e
Yura Kovalchuk <deersblabla@gmail.com>
2023-03-27 01:46:32 +0300

    celsius to fahrenheit

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -3,19 +3,16 @@ import 'dart:io';
 import 'package:dart_core_cli/dart_core_cli.dart' as dart_core_cli;
 
 void main(List<String> arguments) {
-  var number1 = stdin.readLineSync();
-  var number2 = stdin.readLineSync();
-  var number3 = stdin.readLineSync();
-  if (number1 == null || number2 == null || number3 == null) {
+  var celsius = stdin.readLineSync();
+
+  if (celsius == null) {
     print("Sorry, your put value is null");
   } else {
-    double? result1 = double.tryParse(number1);
-    double? result2 = double.tryParse(number2);
-    double? result3 = double.tryParse(number3);
-    if (result1 == null || result2 == null || result3 == null) {
+    double? result1 = double.tryParse(celsius);
+    if (result1 == null) {
       print("sorry i can't tryParse");
     } else {
-      print((result1 + result2 + result3)/3);
+      print(9/5*result1+32);
     }
   }
 }
