f707f3821e861e021bd0b6acf23273058947ece0
Yura Kovalchuk <deersblabla@gmail.com>
2023-03-27 01:26:33 +0300

    arethmetic mean

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -3,9 +3,19 @@ import 'dart:io';
 import 'package:dart_core_cli/dart_core_cli.dart' as dart_core_cli;
 
 void main(List<String> arguments) {
-  String? number1 = stdin.readLineSync();
-  String? number2 = stdin.readLineSync();
-  int? result1 = int.tryParse(number1!);
-  int? result2 = int.tryParse(number2!);
-  print(result1!*result2!);
+  var number1 = stdin.readLineSync();
+  var number2 = stdin.readLineSync();
+  var number3 = stdin.readLineSync();
+  if (number1 == null || number2 == null || number3 == null) {
+    print("Sorry, your put value is null");
+  } else {
+    double? result1 = double.tryParse(number1);
+    double? result2 = double.tryParse(number2);
+    double? result3 = double.tryParse(number3);
+    if (result1 == null || result2 == null || result3 == null) {
+      print("sorry i can't tryParse");
+    } else {
+      print((result1 + result2 + result3)/3);
+    }
+  }
 }
