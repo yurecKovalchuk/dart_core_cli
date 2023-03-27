c37367297609c86bb09104a49954f3ce0f9be791
Yura Kovalchuk <deersblabla@gmail.com>
2023-03-27 13:57:02 +0300

    division by three or five

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 import 'package:dart_core_cli/dart_core_cli.dart' as dart_core_cli;
 
 void main(List<String> arguments) {
-for(int i = 0; i <=100; i++){
-  if(i % 2 == 1){
-    print (i);
+  for (int i = 0; i <= 100; i++) {
+    if (i % 3 & 5 == 0) {
+      print(i);
+    }
   }
-}
 }
