0262b1eb230f518cdcb2dc03713ab278e698b42a
Yura Kovalchuk <deersblabla@gmail.com>
2023-03-27 13:07:48 +0300

    even numbers

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -3,7 +3,9 @@ import 'dart:io';
 import 'package:dart_core_cli/dart_core_cli.dart' as dart_core_cli;
 
 void main(List<String> arguments) {
-  int? number1 = int.parse(stdin.readLineSync()!);
-  int? number2 = int.parse(stdin.readLineSync()!);
-  print(number1 % number2);
+for(int i = 0; i <=100; i++){
+  if(i % 2 == 0){
+    print (i);
+  }
+}
 }
