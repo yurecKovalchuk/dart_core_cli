dbc4e914a1d0148d9ff9bcce14538a8327a9d0d6
Yura Kovalchuk <deersblabla@gmail.com>
2023-03-27 13:13:26 +0300

    non-even numbers

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -4,7 +4,7 @@ import 'package:dart_core_cli/dart_core_cli.dart' as dart_core_cli;
 
 void main(List<String> arguments) {
 for(int i = 0; i <=100; i++){
-  if(i % 2 == 0){
+  if(i % 2 == 1){
     print (i);
   }
 }
