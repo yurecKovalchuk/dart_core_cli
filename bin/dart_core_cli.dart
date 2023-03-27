32e33494e739ebb4fd52ee6d52b8cc57754d7bfd
Yura Kovalchuk <deersblabla@gmail.com>
2023-03-27 12:44:22 +0300

    fraction from division

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -3,16 +3,7 @@ import 'dart:io';
 import 'package:dart_core_cli/dart_core_cli.dart' as dart_core_cli;
 
 void main(List<String> arguments) {
-  var celsius = stdin.readLineSync();
-
-  if (celsius == null) {
-    print("Sorry, your put value is null");
-  } else {
-    double? result1 = double.tryParse(celsius);
-    if (result1 == null) {
-      print("sorry i can't tryParse");
-    } else {
-      print(9/5*result1+32);
-    }
-  }
+  int? number1 = int.parse(stdin.readLineSync()!);
+  int? number2 = int.parse(stdin.readLineSync()!);
+  print(number1 % number2);
 }
