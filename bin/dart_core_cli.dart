78e968bebc72d084bd4e2e0d30393ff591e6cf65
Yura Kovalchuk <deersblabla@gmail.com>
2023-03-28 12:22:50 +0300

    guess the number version two

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -5,16 +5,21 @@ import 'package:dart_core_cli/dart_core_cli.dart' as dart_core_cli;
 
 void main(List<String> arguments) {
   var seed = Random(DateTime.now().millisecondsSinceEpoch).nextInt(100);
+  var counter = 0;
   while (true) {
     print("вгадай число");
     int? number = int.parse(stdin.readLineSync()!);
+    counter++;
     if (number > seed) {
       print("Бери меньше");
     } else if (number < seed) {
       print("Бери більше");
     } else {
       print("Вгадав");
+      print("$counter");
+      print("Спробуйте ще раз");
       break;
     }
   }
 }
+
