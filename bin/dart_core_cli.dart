8f61908f51fbedd10f958786459e7e6b794ef72b
Yura Kovalchuk <deersblabla@gmail.com>
2023-03-29 14:41:20 +0300

    used cycle for

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -5,11 +5,11 @@ import 'package:dart_core_cli/dart_core_cli.dart' as dart_core_cli;
 import 'package:test/expect.dart';
 
 void main(List<String> arguments) {
-  print("введіть слово");
-  String? word = stdin.readLineSync()!;
-  var s = "";
-  var st = word.codeUnits;
-  var st2 = st.reversed;
-  var st3 = String.fromCharCodes(st2);
-  print(st3);
+  print("ведіть текст");
+  String? str = stdin.readLineSync()!;
+  var strRev = "";
+  for (int i = str.length - 1; i >= 0; i--) {
+    strRev += str[i];
+  }
+  print(strRev);
 }
