fae1f39cc4891bf6778cb52ca313d02c90cb9a7c
Yura Kovalchuk <deersblabla@gmail.com>
2023-03-29 13:56:34 +0300

    used method reversed

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -2,24 +2,14 @@ import 'dart:io';
 import 'dart:math';
 
 import 'package:dart_core_cli/dart_core_cli.dart' as dart_core_cli;
+import 'package:test/expect.dart';
 
 void main(List<String> arguments) {
-  var seed = Random(DateTime.now().millisecondsSinceEpoch).nextInt(100);
-  var counter = 0;
-  while (true) {
-    print("вгадай число");
-    int? number = int.parse(stdin.readLineSync()!);
-    counter++;
-    if (number > seed) {
-      print("Бери меньше");
-    } else if (number < seed) {
-      print("Бери більше");
-    } else {
-      print("Вгадав");
-      print("$counter");
-      print("Спробуйте ще раз");
-      break;
-    }
-  }
+  print("введіть слово");
+  String? word = stdin.readLineSync()!;
+  var s = "";
+  var st = word.codeUnits;
+  var st2 = st.reversed;
+  var st3 = String.fromCharCodes(st2);
+  print(st3);
 }
-
