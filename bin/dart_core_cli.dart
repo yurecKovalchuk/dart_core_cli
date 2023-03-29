8fcc1380395026871e49aa87c75cc79020138d63
Yura Kovalchuk <deersblabla@gmail.com>
2023-03-29 14:58:08 +0300

    displays text in capital letters

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -7,9 +7,6 @@ import 'package:test/expect.dart';
 void main(List<String> arguments) {
   print("ведіть текст");
   String? str = stdin.readLineSync()!;
-  var strRev = "";
-  for (int i = str.length - 1; i >= 0; i--) {
-    strRev += str[i];
-  }
-  print(strRev);
+ String? st = str.toUpperCase();
+ print(st);
 }
