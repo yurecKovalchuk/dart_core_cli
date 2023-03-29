b592d0ad088633f4c521414c30284e85cff9b607
Yura Kovalchuk <deersblabla@gmail.com>
2023-03-29 16:24:51 +0300

    checking words for equality

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -5,14 +5,19 @@ import 'package:dart_core_cli/dart_core_cli.dart' as dart_core_cli;
 import 'package:test/expect.dart';
 
 void main(List<String> arguments) {
-  print("Перевірка слова на поліндромність");
+  print("Введіть текст для перевірки на рівність");
   String? str = stdin.readLineSync()!;
+  print("Введіть текст для перевірки на рівність");
+  String? str1 = stdin.readLineSync()!;
+  var st1 = str1.codeUnits;
+  var st11 = st1.length;
   var st = str.codeUnits;
-  var st1 = st.reversed;
-  String st2 = String.fromCharCodes(st1);
-  if (str == st2) {
-    print("поліндром");
-  } else {
-    print("не поліндром");
+  var stt = st.length;
+  print(st11);
+  print(stt);
+  if(st11 == stt){
+    print("Рівні");
+  } else {print("не рівні");
   }
+
 }
