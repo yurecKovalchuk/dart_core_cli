9f4c0ca2c41eaf5f5ac3e3567e8c1bfbb55a38a0
Yura Kovalchuk <deersblabla@gmail.com>
2023-03-31 17:59:11 +0300

    password generation

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -5,39 +5,65 @@ import 'package:dart_core_cli/dart_core_cli.dart' as dart_core_cli;
 import 'package:test/expect.dart';
 
 void main(List<String> arguments) {
-  print(
-      "Яким методом ви хочете це зробити? Якщо простим, натиснгіть 1, якщо складним, натисніть 2");
-  String str = stdin.readLineSync()!;
-  int? st = int.tryParse(str);
-  if (str == null) {
-    print("Не коректно");
-  } else {
-    if (st == 1) {
-      removeSpacesByString();
-    }
-    if (st == 2) {
-      removeSpacesCycle();
-    }
-  }
-}
 
-void removeSpacesByString() {
-  print("Введіть текст");
-  String? str = stdin.readLineSync()!;
-  String? st = str.replaceAll(' ', '');
-  print(st);
-}
+    print("Генерація пароля");
+    print("Вибиріть, що ви хочете, щоби було присутнє в вашому паролі");
+    print("1 - означає 'Так', 0 - значає 'Ні'");
+    print("Великі літери повинні бути присутні в паролі?");
+    String? capitalLetters = stdin.readLineSync()!;
+    print("Маленькі букви повинні бути присутні в паролі?");
+    String? small_letters = stdin.readLineSync()!;
+    print("Цифри повинні бути присутні в паролі?");
+    String? number = stdin.readLineSync()!;
+    print("Спеціальні символи повинні бути присутні в паролі?");
+    String? special_symbols = stdin.readLineSync()!;
+    print("Кількість символів, що має бути в паролі?");
+    String? password_s = stdin.readLineSync()!;
+    int? password_size = int.tryParse(password_s)!;
+    String upper = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+    String lower = 'abcdefghijklmnopqrstuvwxyz';
+    String numbers = '1234567890';
+    String symbols = '!@#\$%^&*()<>,./';
+    int passLength = password_size;
 
-void removeSpacesCycle() {
-  print("Введіть текст");
-  String? str = stdin.readLineSync()!;
-  List<int> inputData = str.codeUnits;
-  List<int> outputData = [];
-  for (int i = 0; i <= inputData.length - 1; i++) {
-    if (inputData[i] != 32) {
-      outputData.add(inputData[i]);
+    String password = '';
+    List<String> list =
+    _generateSeed(capitalLetters, small_letters, number, special_symbols)
+        .split("")
+        .toList();
+    Random rand = Random();
+
+    for (int i = 0; i < passLength; i++) {
+      int index = rand.nextInt(list.length);
+      password += list[index];
     }
+    print(password);
+
   }
-  String output = String.fromCharCodes(outputData);
-  print(output);
-}
+
+  String _generateSeed(
+      String capital_letters,
+      String small_letters,
+      String number,
+      String special_symbols,
+      ) {
+    String upper = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+    String lower = 'abcdefghijklmnopqrstuvwxyz';
+    String numbers = '1234567890';
+    String symbols = '!@#\$%^&*()<>,./';
+    String seed = "";
+
+    seed += _checkPropertyAvailability(capital_letters, upper);
+    seed += _checkPropertyAvailability(small_letters, lower);
+    seed += _checkPropertyAvailability(number, numbers);
+    seed += _checkPropertyAvailability(special_symbols, symbols);
+
+    return seed;
+  }
+
+  String _checkPropertyAvailability(String key,String sedPart) {
+    if (key.contains("1")) {
+      return sedPart;
+    }
+    return "";
+  }
