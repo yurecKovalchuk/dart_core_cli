5950bd4c1eba4b87ec61e03af2def33daa122771
Dmitro Serdun <dmitroserdun@Dmitros-MacBook-Pro.local>
2023-04-03 12:35:04 +0300

    feat: implement Sieve of Eratosthenes

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -1,5 +1,30 @@
-import 'package:dart_core_cli/dart_core_cli.dart' as dart_core_cli;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_core_cli.calculate()}!');
+  print("Please, enter number for check number status");
+  try {
+    final number = _getNumber();
+    final status = _getNumberStatus(number);
+    print("Status number: $status");
+  } catch (_) {
+    print("Invalid input data");
+  }
+}
+
+int _getNumber() {
+  String? input = stdin.readLineSync()!;
+  return int.parse(input);
+}
+
+String _getNumberStatus(int number) {
+  if (number == 1) return 'does not belong to a complex or to a simple number';
+  if (number < 1) return 'domain of non-natural numbers';
+
+  for (int i = 2; i <= number; i++) {
+    final isDivideBySelf = number % i;
+    if (isDivideBySelf == 0 && i != number) {
+      return 'complex number';
+    }
+  }
+  return 'simple';
 }
