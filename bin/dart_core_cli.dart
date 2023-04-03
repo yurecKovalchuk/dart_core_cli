51d95c8cb7277cdc4a3767b45f251a308c5d3803
Yura Kovalchuk <deersblabla@gmail.com>
2023-04-03 14:09:56 +0300

    finding prime numbers

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -1,5 +1,31 @@
+import 'dart:io';
+
 import 'package:dart_core_cli/dart_core_cli.dart' as dart_core_cli;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_core_cli.calculate()}!');
+  print("Введіть цифру");
+  try {
+    final number = _inputNumber();
+    final result = _functionFor(number);
+    print(result);
+  } catch (e) {
+    print("Не вірне введення: $e");
+  }
+}
+
+int _inputNumber() {
+  String input = stdin.readLineSync()!;
+  return int.parse(input);
+}
+
+String _functionFor(int number){
+  if(number == 1) print("Не коректне число");
+  if(number <= 0) print("Не коректне число");
+  for (int i = 2; i <= number; i++){
+    var resultDivision = (number % i);
+    if (resultDivision == 0 && number != i){
+      return "Не просте число";
+    }
+  }
+  return "Просте число";
 }
