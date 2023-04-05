404c7dcb4cdfe89ad7265e238e1e65829b02c2fa
Yura Kovalchuk <deersblabla@gmail.com>
2023-04-05 15:57:42 +0300

    Find the area and circumference of a circle

--- bin/dart_core_cli.dart
+++ bin/dart_core_cli.dart
@@ -3,16 +3,13 @@ import 'dart:io';
 import 'package:dart_core_cli/dart_core_cli.dart' as dart_core_cli;
 
 void main(List<String> arguments) {
-  List<int> numberListt = [];
-  int summ = 0;
-
-  for (int i = 0; i <= 100; i++) {
-    if (i % 3 == 0 || i % 5 == 0) {
-      numberListt.add(i);
-    }
-  }
-  for (int i = 0; i <= numberListt.length-1; i++){
-     summ += numberListt[i];
-  }
-  print (summ);
+  const pi = 3.14159265;
+  print("Введіть радіус кола");
+  String? number = stdin.readLineSync()!;
+  double radius = double.tryParse(number)!;
+  double radiusSquared = (radius * radius);
+  double S = radiusSquared * pi;
+  double I = 2 * pi * radius;
+  print("Площа кола: $S");
+  print("Довжина окружності: $I");
 }
